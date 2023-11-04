131d486557c42ee22675311df86ea9629cd0225a
RobPruzan <rapruzan@buffalo.edu>
2023-11-04 12:30:16 -0400

    prettier stuff

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const index = (props: Props) => {
   return (
     <div className="w-screen h-screen">
-      <div className="w-full h-16 border-b "></div>
+      <div className="w-full h-16 border-b"></div>
       <div className="w-full h-16 border-b "></div>
       <div className="w-full h-16 border-b "></div>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
