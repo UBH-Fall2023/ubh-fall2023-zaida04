98d0f691b0b0a6fb54afd573720271f5bdfc226a
RobPruzan <rapruzan@buffalo.edu>
2023-11-04 16:22:39 -0400

    store stuff

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images:
+    // catchall
+    {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const modifyQuery = (
+  setLoc: (...args: any[]) => void,
+  newQuery: string[],
+) => {
+  setLoc((prev: any) => ({
+    ...prev,
+    searchParams: new URLSearchParams([newQuery]),
+  }));
+};
