0046b14275a8bf0be25befc1eb730919a2341dfb
nico <nico.03727+github@gmail.com>
2023-11-04 12:40:18 -0400

    feat: drizzle

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,11 +1,4 @@
-import {
-  pgTable,
-  serial,
-  text,
-  varchar,
-  timestamp,
-  integer,
-} from "drizzle-orm/pg-core";
+import { pgTable, serial, text, integer, json } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -38,22 +31,23 @@ export type Item = typeof items.$inferSelect;
 
 export const orders = pgTable("orders", {
   id: serial("id").primaryKey(),
-  items: 
-  ordererId: serial("ordererId").references(users, "id").notNull(),
+  items: serial("items").array().notNull(),
+  ordererId: serial("ordererId"),
 });
 export type Order = typeof orders.$inferSelect;
 
 export const messages = pgTable("messages", {
   id: serial("id").primaryKey(),
-  senderId: serial("senderId").references(users, "id").notNull(),
-  receiverId: serial("receiverId").references(users, "id").notNull(),
+  content: text("content").notNull(),
+  senderId: serial("senderId"),
+  receiverId: serial("receiverId"),
 });
 export type Message = typeof messages.$inferSelect;
 
 export const rooms = pgTable("rooms", {
   id: serial("id").primaryKey(),
   // For foreign key arrays, assuming the library allows for an array of references
-  chatterIDs: array(serial("chatterIDs")).references(users, "id"),
-  messageIDs: array(serial("messageIDs")).references(messages, "id"),
+  chatterIDs: serial("chatterIDs").array().notNull(),
+  messageIDs: serial("messageIDs").array().notNull(),
 });
 export type Room = typeof rooms.$inferSelect;
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,31 +1,51 @@
-import 'dotenv/config';
-import { makeDB } from '../db/client';
-import { users, projects, logs } from './drizzle';
-import { eq } from 'drizzle-orm';
+import "dotenv/config";
+import { makeDB } from "../db/client";
+import { items, messages, restaurants, users } from "./drizzle";
+import { faker } from "@faker-js/faker";
 
 const { client, db } = makeDB(process.env.DATABASE_URL!);
 
 async function main() {
-	await client.insert(users).values({
-		id: 1,
-		email: 'nico.03727@gmail.com',
-		name: 'Zaid'
-	});
+  await client.insert(users).values({
+    email: "nico.03727@gmail.com",
+    first_name: "Zaid",
+    last_name: "Arshad",
+    password: "IAMAPASSWORD",
+  });
 
-	await client.insert(projects).values({
-		id: 1,
-		name: 'My First Project',
-		ownerId: 1
-	});
+  for (let i = 0; i < 10; i++) {
+    await client.insert(users).values({
+      email: faker.internet.email(),
+      first_name: faker.person.firstName(),
+      last_name: faker.person.lastName(),
+      password: faker.internet.password(),
+    });
+  }
 
-	await client.insert(logs).values({
-		id: 1,
-		projectId: 1,
-		generatedAt: new Date(),
-		receivedAt: new Date(new Date().getTime() + 1000),
-		content: 'Hello World!'
-	});
-	db.end();
+  for (let i = 0; i < 10; i++) {
+    await client.insert(restaurants).values({
+      name: faker.company.name(),
+      stars: faker.number.int({ min: 1, max: 5 }),
+    });
+  }
+
+  for (let i = 0; i < 10; i++) {
+    await client.insert(items).values({
+      name: faker.commerce.productName(),
+      restaurantId: faker.number.int({ min: 1, max: 10 }),
+      description: faker.commerce.productDescription(),
+    });
+  }
+
+  for (let i = 0; i < 10; i++) {
+    await client.insert(messages).values({
+      content: faker.lorem.sentence(),
+      senderId: faker.number.int({ min: 1, max: 10 }),
+      receiverId: faker.number.int({ min: 1, max: 10 }),
+    });
+  }
+
+  db.end();
 }
 
 main();
