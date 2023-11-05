7f91a3803fa564595d5a5f984fdf636807e9b3f4
nico <nico.03727+github@gmail.com>
2023-11-05 00:24:38 -0400

    fix: i hate dbs

---
+++ drizzle/0005_tired_metal_master.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "orders" SET COLUMN "delivererId" uuid;--> statement-breakpoint
+ALTER TABLE "orders" ADD COLUMN "status" text;
--- drizzle/0006_fresh_maggott.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "orders" ALTER COLUMN "delivererId" DROP NOT NULL;
