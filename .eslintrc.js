854f7c55643d1d016405d5d39ca3bb23ec7fa5d3
nico <nico.03727+github@gmail.com>
2023-11-05 11:01:50 -0500

    ci: add linting

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "plugin:react/recommended",
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {}
+}
