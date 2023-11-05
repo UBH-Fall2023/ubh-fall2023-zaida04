b55f2ee8ebd49bd15536ce935d325cec84b5524f
nico <nico.03727+github@gmail.com>
2023-11-04 23:27:28 -0400

    fix: appease vercel gods

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -7,53 +7,58 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 import { useQuery } from "@tanstack/react-query";
-import { Checkbox } from "@/components/ui/checkbox"
-import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 
-type Props = {
-
-};
+type Props = {};
 
 export default function MainPage(props: Props) {
   const [selectedOrders, setSelectedOrders] = useState<string[]>([]);
 
   const query = useQuery({
-    queryFn: async () => await (await fetch('/api/orders')).json(),
-    queryKey: ["orders"]
-  })
+    queryFn: async () => await (await fetch("/api/orders")).json(),
+    queryKey: ["orders"],
+  });
 
   const selectOrder = (c: string | Boolean, orderId: string) => {
     if (c) {
       if (selectedOrders.length === 3) {
-        return console.log('Reached max capacity of order pickups!')
+        return console.log("Reached max capacity of order pickups!");
       } else {
-        return setSelectedOrders([...selectedOrders, orderId])
+        return setSelectedOrders([...selectedOrders, orderId]);
       }
     }
 
-    return setSelectedOrders(selectedOrders.filter(item => item !== orderId))
-  }
+    return setSelectedOrders(selectedOrders.filter((item) => item !== orderId));
+  };
 
   const claimOrder = async () => {
-    const claimOrderStatus = await fetch('/api/orders', )
-    console.log('clicked')
-  }
+    const claimOrderStatus = await fetch("/api/orders");
+    console.log("clicked");
+  };
 
   useEffect(() => {
     console.log(selectedOrders);
-  }, [selectedOrders])
+  }, [selectedOrders]);
 
   return (
     <div className="h-max flex flex-col items-center justify-center">
       <h1 className="my-8 font-bold text-xl">Available Orders</h1>
-      <Button disabled={selectedOrders.length === 0} onClick={claimOrder}>I'm Ready</Button>
-      <div className={`h-4/6 mt-3 w-5/6 ${query.isLoading || query.isError ? 'text-center' : null}`}>
+      <Button disabled={selectedOrders.length === 0} onClick={claimOrder}>
+        I'm Ready
+      </Button>
+      <div
+        className={`h-4/6 mt-3 w-5/6 ${
+          query.isLoading || query.isError ? "text-center" : null
+        }`}
+      >
         {query.isLoading && <h1>Loading...</h1>}
-        {query.isError && <h1>Oops! An error occured, please try refreshing the page.</h1>}
-        {query.data && query.data.data && query.data.data.length > 0
-          ?
+        {query.isError && (
+          <h1>Oops! An error occured, please try refreshing the page.</h1>
+        )}
+        {query.data && query.data.data && query.data.data.length > 0 ? (
           <Table className="min-w-full mt-2 border border-gray-300">
             <TableCaption>A list of orders you can pick up.</TableCaption>
             <TableHeader>
@@ -67,12 +72,17 @@ export default function MainPage(props: Props) {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {query.data.data.map((orderInfo, idx) => {
+              {query.data.data.map((orderInfo: any, idx: any) => {
                 return (
                   <TableRow key={idx}>
                     <TableCell className="font-medium">
                       <div className="flex gap-2">
-                        <Checkbox onCheckedChange={(c) => selectOrder(c, orderInfo.id)} className="my-auto"/>
+                        <Checkbox
+                          onCheckedChange={(c: any) =>
+                            selectOrder(c, orderInfo.id)
+                          }
+                          className="my-auto"
+                        />
                         <p>Claim</p>
                       </div>
                     </TableCell>
@@ -82,12 +92,14 @@ export default function MainPage(props: Props) {
                     <TableCell>5 minutes away</TableCell>
                     <TableCell>3m 7s</TableCell>
                   </TableRow>
-              )})}
+                );
+              })}
             </TableBody>
           </Table>
-        : query.isLoading ? null : <span>No orders to be fulfilled...(yet!)</span>
-        }
+        ) : query.isLoading ? null : (
+          <span>No orders to be fulfilled...(yet!)</span>
+        )}
       </div>
     </div>
-  )
+  );
 }
