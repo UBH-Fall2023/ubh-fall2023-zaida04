d954aba66fce34a38cb94d1a50a1154d0c45e9d3
Ramin <rramin125@gmail.com>
2023-11-04 23:14:41 -0400

    rm unnecessary lines

--- src/pages/api/orders/index.ts
+++ src/pages/api/orders/index.ts
@@ -25,24 +25,7 @@ export default async function handler(
 
   return res.status(200).json({ message: "Hello from Next.js!"});
 
-
-  return res.status(404)
-
-
-  // Create a route to grab all orders
-
   // Create a route to accept/claim order
 
   // Create a route to send updates to order
-
-  // const queriedItems = await client.query.items.findMany()
-
-  // await client.insert(orders).values({
-  //   orderTotal: "10.79",
-  //   tips: "3.00",
-  //   items: queriedItems.map(item => item.id).slice(0, 3),
-  //   ordererId: "b527cd9b-3811-4cb7-ae5c-44fcafed3d3e",
-  // })
-
-  res.status(200).json({ message: "Hello from Next.js!"});
 }
